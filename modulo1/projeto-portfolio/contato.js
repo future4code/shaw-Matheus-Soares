505e9a3f336170610f195d47455ed4169d4593b6
matheus <matesuboy@gmail.com>
2022-03-11 17:57:06 -0300

    implementado formulario para contato

---
+++ modulo1/projeto-portfolio/contato.js
@@ -0,0 +1,6 @@
+botao = confirm("Deseja deixar uma mensagem?")
+if(botao===true){
+    nome = prompt("Digite o seu nome:")
+    email = prompt("Digite o seu email:")
+    mensagem = prompt("Deixe uma mensagem:")
+}
