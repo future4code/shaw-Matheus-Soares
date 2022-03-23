4efab16bb319750a4a3a6857a0b0643427a3e2e3
matheus <matesuboy@gmail.com>
2022-03-23 16:24:34 -0300

    exercicio componentes de classe

---
+++ modulo2/estados-insta4/insta4/src/App.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import styled from 'styled-components'
+import Post from './components/Post/Post';
+
+const MainContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  align-items: center;
+`
+
+class App extends React.Component {
+  render() {
+    return (
+      <><MainContainer>
+        <Post
+          nomeUsuario={'paulinha'}
+          fotoUsuario={'https://picsum.photos/50/50'}
+          fotoPost={'https://picsum.photos/200/150'} />
+      </MainContainer><MainContainer>
+          <Post
+            nomeUsuario={'matheus'}
+            fotoUsuario={'https://picsum.photos/10/50'}
+            fotoPost={'https://picsum.photos/203/150'} />
+        </MainContainer><MainContainer>
+          <Post
+            nomeUsuario={'maria'}
+            fotoUsuario={'https://picsum.photos/20/50'}
+            fotoPost={'https://picsum.photos/201/150'} />
+        </MainContainer></>
+    );
+  }
+}
+
+export default App;
