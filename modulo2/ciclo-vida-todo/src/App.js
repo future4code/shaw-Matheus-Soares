f47e16e74a2678343a3a5ebd48cfc8b11c38d8c7
matheus <matesuboy@gmail.com>
2022-03-29 18:37:54 -0300

    ...

--- modulo2/ciclo-vida-todo/src/App.js
+++ modulo2/ciclo-vida-todo/src/App.js
@@ -10,19 +10,40 @@ const Button = styled.button`
 const Select = styled.select`
   margin-left: 10px;
 `
+const Botao = styled.button`
+  border-style: none;
+  background-color: white;
+`
 
 export default class App extends React.Component {
   state = {
-    tarefa: '',
-    inputTarefa: '',
-    filtro: ''
+      tarefas: [],
+      inputTarefa: '',
+      filtro: ''
+  }
+
+  adicionaTarefa = () => {
+    const tarefa = {
+      id: Date.now(),
+      texto: this.state.inputTarefa,
+      completa: false
+    }
+    const tarefa2 = [...this.state.tarefas, tarefa]
+    this.setState({ tarefas: tarefa2 })
+    this.setState({ inputTarefa: '' })
+  }
+
+  riscar = () => {
+    return (
+      this.setState({completa: true })
+    )
   }
 
   onChangeInputTarefa = (event) => {
     this.setState({ inputTarefa: event.target.value })
   }
 
-  listaDeTarefas = () => {
+  funcaoPrincipal = () => {
     return (
       <>
         <h1>Lista de tarefas</h1>
@@ -30,7 +51,7 @@ export default class App extends React.Component {
           value={this.state.inputTarefa}
           onChange={this.onChangeInputTarefa}
         />
-        <Button>Adicionar</Button>
+        <Button onClick={this.adicionaTarefa}>Adicionar</Button>
         <p>Filtro
           <Select>
             <option>Nenhum</option>
@@ -42,11 +63,29 @@ export default class App extends React.Component {
     )
   }
 
+  renderizar = (id, tarefa) => {
+    if (id===tarefa.id){
+      console.log('entrou no if');
+      return <Botao onClick={this.riscar}>{tarefa.inputTarefa}</Botao>
+    }
+    return <Botao onClick={this.riscar}><s>{tarefa.inputTarefa}</s></Botao>
+  }
+
   render() {
+    const ListaDeTarefas = this.state.tarefas.map((tarefa) => {
+      return (
+        <li>
+          <Botao onClick={this.renderizar(tarefa.id, tarefa)}>{tarefa.texto}</Botao>.
+        </li>
+      )
+    })
     return (
       <Main>
         <div>
-          {this.listaDeTarefas()}
+          {this.funcaoPrincipal()}
+        </div>
+        <div>
+          {ListaDeTarefas}
         </div>
       </Main>
     )
