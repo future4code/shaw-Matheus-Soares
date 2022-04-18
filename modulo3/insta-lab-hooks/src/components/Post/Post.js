3e2c0d9ed3b6ae60a35dc227368703cbe957b2ff
matheus <matesuboy@gmail.com>
2022-04-18 19:20:15 -0300

    exercicio insta-lab-hooks finalizado

--- modulo3/insta-lab-hooks/src/components/Post/Post.js
+++ modulo3/insta-lab-hooks/src/components/Post/Post.js
@@ -9,40 +9,72 @@ import iconeCoracaoPreto from '../../img/favorite.svg'
 import iconeComentario from '../../img/comment_icon.svg'
 
 const Post = (props) => {
+  const [curtir, setCurtir] = useState(false)
+  const [numeroDeCurtidas, setNumeroDeCurtidas] = useState(0)
+  const [comentar, setComentar] = useState(false)
+  const [numeroDeComentarios, setNumeroDeComentarios] = useState(0)
+  const [comentarios, setComentarios] = useState([])
 
 
   const onClickCurtida = () => {
+    if (curtir) {
+      setCurtir(!curtir)
+      setNumeroDeCurtidas(numeroDeCurtidas - 1)
+    }
+    else {
+      setCurtir(!curtir)
+      setNumeroDeCurtidas(numeroDeCurtidas + 1)
+    }
   };
 
   const onClickComentario = () => {
+    setComentar(!comentar)
   };
 
   const enviarComentario = (comentario) => {
+    const listaDeComentarios = [...comentarios, comentario]
+    setComentarios(listaDeComentarios)
+    setComentar(false)
+    setNumeroDeComentarios(numeroDeComentarios + 1)
   }
+
+  const iconeCurtir = curtir ? (iconeCoracaoPreto) : (iconeCoracaoBranco)
+
+  const caixaDeComentario = comentar ? (
+    <SecaoComentario enviarComentario={enviarComentario} />
+  ) : (
+    comentarios.map(comment => {
+      return (
+        <CommentContainer>
+          <p>{comment}</p>
+        </CommentContainer>
+      )
+    })
+  )
 
   return (
     <PostContainer>
       <PostHeader>
-        <UserPhoto src={props.fotoUsuario} alt={'Imagem do usuario'}/>
+        <UserPhoto src={props.fotoUsuario} alt={'Imagem do usuario'} />
         <p>{props.nomeUsuario}</p>
       </PostHeader>
 
-      <PostPhoto src={props.fotoPost} alt={'Imagem do post'}/>
+      <PostPhoto src={props.fotoPost} alt={'Imagem do post'} />
 
       <PostFooter>
         <IconeComContador
-          // icone={iconeCurtida}
+          icone={iconeCurtir}
           onClickIcone={onClickCurtida}
-          // valorContador={numeroCurtidas}
+          valorContador={numeroDeCurtidas}
         />
 
         <IconeComContador
           icone={iconeComentario}
           onClickIcone={onClickComentario}
-          // valorContador={numeroComentarios}
+          valorContador={numeroDeComentarios}
         />
       </PostFooter>
-      {/* {caixaDeComentario} */}
+      {caixaDeComentario}
     </PostContainer>
   )
 }
