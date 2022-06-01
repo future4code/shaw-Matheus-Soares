4087df1478967857cee32229898059e2a2ecda25
matheus <matesuboy@gmail.com>
2022-06-01 17:43:13 -0300

    exercicio de fluxo de dados backend

---
+++ modulo4/fluxo-de-dados-backend/src/data.ts
@@ -0,0 +1,23 @@
+export type algo = {
+    id: string,
+    name: string,
+    price: number
+}
+
+export const produtos: algo[] = [
+    {
+        id: 'asdf-lkj-wqert-vxbc',
+        name: 'macarrao',
+        price: 5.00
+    },
+    {
+        id: 'khjl-poui-dghf',
+        name: "creme de leite",
+        price: 4.00
+    },
+    {
+        id: 'qwer-fgdh-ytiu',
+        name: 'leite-condensado',
+        price: 7.00
+    }
+]
