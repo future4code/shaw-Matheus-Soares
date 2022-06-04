7c747030ebcc821cc99ac202b9db3ab04981d404
matheus <matesuboy@gmail.com>
2022-06-03 22:37:58 -0300

    ...

---
+++ modulo4/apis-rest/src/index.ts
@@ -0,0 +1,75 @@
+import express from "express"
+import cors from "cors"
+
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+
+type User = {
+    id: number,
+    name: string,
+    email: string,
+    type: string,
+    age: number
+}
+
+let users = [
+    {
+        id: 1,
+        name: "Alice",
+        email: "alice@email.com",
+        type: "ADMIN",
+        age: 12
+    },
+    {
+        id: 2,
+        name: "Bob",
+        email: "bob@email.com",
+        type: "NORMAL",
+        age: 36
+    },
+    {
+        id: 3,
+        name: "Coragem",
+        email: "coragem@email.com",
+        type: "NORMAL",
+        age: 21
+    },
+    {
+        id: 4,
+        name: "Dory",
+        email: "dory@email.com",
+        type: "NORMAL",
+        age: 17
+    },
+    {
+        id: 5,
+        name: "Elsa",
+        email: "elsa@email.com",
+        type: "ADMIN",
+        age: 17
+    },
+    {
+        id: 6,
+        name: "Fred",
+        email: "fred@email.com",
+        type: "ADMIN",
+        age: 60
+    }
+]
+
+//exercicio 1
+// letra a - metodo get
+// letra b - "/users"
+
+//exercicio 2
+
+
+
+
+
+
+
+
+app.listen(3003, () => console.log("Servidor disponível em 3003"))
