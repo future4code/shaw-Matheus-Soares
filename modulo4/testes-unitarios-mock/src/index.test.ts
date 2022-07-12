e46f84913cba1e4bde6b0b99fca67780a4f03108
matheus <matesuboy@gmail.com>
2022-07-12 19:05:56 -0300

    exercicio de testes unitarios

---
+++ modulo4/testes-unitarios-mock/src/index.test.ts
@@ -0,0 +1,10 @@
+test("Should return false for empty name", () => {
+    const result = validateCharacter({
+        name: "",
+        life: 1500,
+        strength: 300,
+        defense: 500,
+    });
+
+    expect(result).toBe(false);
+});
