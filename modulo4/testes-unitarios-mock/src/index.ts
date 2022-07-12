3e5c44705e47acde22cdb9b0463dddd0ac358232
matheus <matesuboy@gmail.com>
2022-07-12 19:23:37 -0300

    ...

--- modulo4/testes-unitarios-mock/src/index.ts
+++ modulo4/testes-unitarios-mock/src/index.ts
@@ -2,17 +2,41 @@ import { Character } from "./types";
 
 export const validateCharacter = (input: Character): boolean => {
    if (
-     !input.name ||
-       input.life === undefined || 
-     input.strength === undefined ||
-     input.defense === undefined
+      !input.name ||
+      input.life === undefined ||
+      input.strength === undefined ||
+      input.defense === undefined
    ) {
-     return false;
+      return false;
    }
- 
-   if (input.life <=0 || input.strength <=0 || input.defense <=0) {
-     return false;
+
+   if (input.life <= 0 || input.strength <= 0 || input.defense <= 0) {
+      return false;
    }
- 
+
    return true;
- };
+};
+
+// export const performAttack = (attacker: Character, defender: Character) => {
+//    if (!validateCharacter(attacker) || !validateCharacter(defender)) {
+//       throw new Error("Invalid character");
+//    }
+
+//    if (attacker.strength > defender.defense) {
+//       defender.life -= attacker.strength - defender.defense;
+//    }
+// };
+
+export const performAttack = (
+   attacker: Character,
+   defender: Character,
+   validator: (input: Character) => boolean
+) => {
+   if (!validator(attacker) || !validator(defender)) {
+      throw new Error("Invalid character");
+   }
+
+   if (attacker.strength > defender.defense) {
+      defender.life -= attacker.strength - defender.defense;
+   }
+};
