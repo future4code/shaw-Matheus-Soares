23aa4bab7aeb456cdcaa7c6540f84aa7a6b8a763
matheus <matesuboy@gmail.com>
2022-08-02 19:15:25 -0300

    ops, branch errada

--- modulo6/numero faltante/src/index.ts
+++
@@ -1,8 +0,0 @@
-export const findMissingNumber = (arr: number[]): number => {
-    const expectedSum = 5050;
-    let sum = 0;
-    for (const num of arr) {
-      sum += num;
-    }
-    return expectedSum - sum;
-  };
